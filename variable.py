177c6514aca88e13074f4f2c924b5c874218be06
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-08-19 20:00:54 +0530

    variables completed

---
+++ variable.py
@@ -0,0 +1,65 @@
+# Variables:-
+
+    # A container to store data ,
+    # A Reference to access data,
+    # A context to get data,
+    
+    # Ex: myname, myage, email, salary
+    
+# myname = "Shivkumar"
+# myage = 32
+# email = "shivkumar@gmail.com"
+# salary = 5000
+
+# print(myname)
+# print(myage)
+# print(email)
+
+# Rules to create a variable:-
+
+
+    # 1 variable can contain alphabets [uppercase / lowercase]
+    # 2 variable can contain numbers but not at start of variable.
+    # 3 variable should not contain special charecters except _.
+    # 4 variable should not be a keyword.
+    
+# what is keyword:-
+     # keywords are nothing but those words which
+     # python uses internally to operate.
+     
+import keyword
+print(keyword.kwlist)
+
+# 'False', 'None', 'True', 'and', 'as', 
+# 'assert', 'async', 'await', 'break', 
+# 'class', 'continue', 'def', 'del', 
+# 'elif', 'else', 'except', 'finally', 
+# 'for', 'from', 'global', 'if', 'import', 
+# 'in', 'is', 'lambda', 'nonlocal', 'not', 
+# 'or', 'pass', 'raise', 'return', 'try', 
+# 'while', 'with', 'yield'
+
+
+# Exercise :-
+
+fav_book = "One Indian Girl"
+
+_my_book = "One Indian Boy"
+
+__MY_BOOK = "One Indian Kid"
+
+# Python is Case Sensetive Language.
+name = "Shailaja"
+
+NAME =  "Shailoo"
+########################
+
+# 90_Acre = "Propery"
+Acre_90 = "Property"
+
+########################
+
+# my_buddy_@ = "Ramyashri"
+
+
+
