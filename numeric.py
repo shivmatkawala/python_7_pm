8634e92f52fcec25c8fda237eda7b06b2c533f6a
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-08-20 19:58:55 +0530

    added numberic datatypes

---
+++ numeric.py
@@ -0,0 +1,59 @@
+# INT DATATYPE:-
+
+# x = 1
+# print(x)
+# print(type(x))
+# y = -1
+# print(y)
+# print(type(y))
+# z = 0
+# print(z)
+# print(type(z))
+# m = 12
+# print(m)
+# print(type(m))
+# n = -56
+# print(n)
+# print(type(n))
+
+# FLOAT DATATYPE:-
+
+# a = 1.0
+# print(a)
+# print(type(a))
+
+# b = -1.0
+# print(b)
+# print(type(b))
+
+# c = 0.0
+# print(c)
+# print(type(c))
+
+# d = 123.567
+# print(d)
+# print(type(d))
+
+# e = -456.78
+# print(e)
+# print(type(e))
+
+
+# COMPLEX DATATYPES:-
+
+# p = 1 + 1j
+# print(p)
+# print(type(p))
+
+# q = -1 -1j
+# print(q)
+# print(type(q))
+
+# r = 0.0 - 0j
+# print(r)
+# print(type(r))
+
+# s = 123 +  456j
+# print(s)
+# print(type(s))
+
