235f7e7d1e97a9822c022e9db1ed163aaa245f51
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-08-21 19:52:36 +0530

    arithmetic operations

---
+++ arithmetic_ops.py
@@ -0,0 +1,63 @@
+# ARITHMETIC OPERATIONS:-
+
+# 3 + 6 = 9
+
+# What is expression ?
+# what is operator ?
+# what is operand ?
+
+
+    # 1 Addition:-  +
+# x = 9
+# y = 2
+# print("addition:-")
+# print(x + y)
+
+    # 2 Substraction:-  -
+# print("Substraction:-")
+# print(x - y)
+
+#     # 3 Mulltiplication:-  *
+# print("Multiplication")
+# print(x * y)
+
+    # 4 Division:-
+        # 1) Normal Division:-  /
+# print("Normal Division")
+# print(x / y)
+
+#         # 2) Floor Division:-  //
+# print("Floor Division") 
+# print(x // y)
+ 
+    # Exponent/ power:- **
+# print("Power:-")
+# print(x ** y)
+    
+    # Mod Operation:-  %
+# print("Mod Operation")
+# print(x % y)
+
+
+m = 7.3
+n = 2.2
+
+p = 4+8j
+q = 2+4j
+
+
+# complex Numbers:-
+p = 4+8j
+q = 2+4j
+
+print(f"Addition of p and q is {p + q}")
+print(f"Substraction of p and q is {p - q}")
+print(f"Multiplication of p and q is {p * q}")
+print(f"Normal Division of p and q is {p / q}")
+# print(f"Floor Division of p and q is {p // q}") not supported
+print(f"power: {p ** q}")
+# print(f"Mod Ops: {p % q}")  not supported
+
+
+# num = 100
+# print(f"number is {num}")
