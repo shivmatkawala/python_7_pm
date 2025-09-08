0409d39b329b13cb8457ef27c77a0e57dae4a964
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-09-08 19:59:34 +0530

    added files

---
+++ ans1.py
@@ -0,0 +1,23 @@
+# str1 = "AMANDA HAS GONE"
+
+# # 'Go home'
+# str2 = str1[-4] + str1[-3] + " " + str1[7] + str1[-3] + str1[1] + str1[-1]
+# print(str2.capitalize())
+
+
+# # "mahesH"
+# str3 = str1[1] + str1[0] + str1[7] + str1[-1] + str1[-6] 
+# print(str3.lower() + str1[7].upper())
+
+
+# # 
+# s1 = set(str1)
+# print(list(s1))
+
+
+list1 = [123, 56, 34, 89, -5, -234]
+print(sum(list1))  # 63
+print(min(list1))  # -234
+print(max(list1))
+list1.sort()
+print(list1)
---
+++ dictionaryDatatype.py
@@ -0,0 +1,13 @@
+myname = "Shivkumar"
+print(myname)
+
+
+grocerry = ["Oil", "Sugar", 'Maida Pindi', "Soap", "Lentels"]
+print(grocerry)
+
+
+ahemad = {
+    'height' : 5,
+    'weight': 100,
+    'color': 'Whetish',
+}
